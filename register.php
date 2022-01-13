e5b420a11ff07d0afa4243ce6f0eeece62753829
Stuart McCann <81535116+smccsw@users.noreply.github.com>
2022-01-13 22:07:30 +0000

    Update register.php
    Added error messages for required fields within the registration form. Created function to check date format - still to be tested.

--- register.php
+++ register.php
@@ -13,6 +13,13 @@ if(isset($_REQUEST['register_btn'])){
 		print_r($_REQUEST);
 	echo "</pre>";
 
+	//  Possible date check function - to be tested
+	//  function validateDate($date, $format = 'd-m-Y') {
+	// 	$d = DateTime::createFromFormat($format, $date);
+	// 	return $d && $d->format($format) === $date;
+	// }
+
+
 
 	$first_name = filter_var($_REQUEST['first_name'],FILTER_SANITIZE_STRING);
 	$middle_name = filter_var($_REQUEST['middle_name'],FILTER_SANITIZE_STRING);
@@ -21,13 +28,38 @@ if(isset($_REQUEST['register_btn'])){
 	$nino = $_REQUEST['nino']; //regex required
 	$email = filter_var($_REQUEST['email'],FILTER_SANITIZE_EMAIL);
 	$password = strip_tags($_REQUEST['password']);
+	
+
+
 
 	if(empty($first_name)) {
-		$errorMsg[0][] = 'First name required';
+		$errorMsg[0][] = 'First Name required';
 	}
 
 	if(empty($surname)) {
 		$errorMsg[1][] = 'Surname required';
+	}
+
+	if(empty($dob)) {
+		$errorMsg[2][] = 'Date of Birth required';
+	}
+
+	if(empty($nino)) {
+		$errorMsg[3][] = 'National Insurance Number required';
+	}
+
+	if(empty($email)) {
+		$errorMsg[4][] = 'Email address required';
+	}
+
+	if(empty($password)) {
+		$errorMsg[5][] = 'Password required';
+	}
+
+	//more checks required - a-z, A-Z, 0-9 etc.
+
+	if(strlen($password) < 8)  {
+		$errorMsg[5][] = 'Must be at least 8 characters and contain uppercase, lowercase, number and special characters';
 	}
 }
 
@@ -52,6 +84,15 @@ if(isset($_REQUEST['register_btn'])){
 			<div class="mb-3">
 				<label for="first_name" class="form-label">First Name</label>
 				<input type="text" name="first_name" class="form-control" placeholder="Jane">
+				
+				<?php
+					if(isset($errorMsg[0])) {
+						foreach($errorMsg[0] as $firstNameErrors) {
+							echo "<p class='small text-danger'>".$firstNameErrors."</p>";
+						}
+					}
+				?>
+
 			</div>
 			<div class="mb-3">
 				<label for="middle_name" class="form-label">Middle Name</label>
@@ -60,26 +101,67 @@ if(isset($_REQUEST['register_btn'])){
 			<div class="mb-3">
 				<label for="surname" class="form-label">Surname</label>
 				<input type="text" name="surname" class="form-control" placeholder="Doe">
+								
+				<?php
+					if(isset($errorMsg[1])) {
+						foreach($errorMsg[1] as $surnameErrors) {
+							echo "<p class='small text-danger'>".$surnameErrors."</p>";
+						}
+					}
+				?>
+
 			</div>
 			<div class="mb-3">
 				<label for="dob" class="form-label">Date of Birth</label>
 				<input type="date" name="dob" class="form-control" placeholder="dd/mm/yyyy">
+				
+				<?php
+					if(isset($errorMsg[2])) {
+						foreach($errorMsg[2] as $dobErrors) {
+							echo "<p class='small text-danger'>".$dobErrors."</p>";
+						}
+					}
+				?>
 
 			</div>
 			<div class="mb-3">
 				<label for="nino" class="form-label">National Insurance Number</label>
 				<input type="text" name="nino" class="form-control" placeholder="eg. AA112233B">
+				
+				<?php
+					if(isset($errorMsg[3])) {
+						foreach($errorMsg[3] as $ninoErrors) {
+							echo "<p class='small text-danger'>".$ninoErrors."</p>";
+						}
+					}
+				?>
 
 			</div>
 			<div class="mb-3">
 				<label for="email" class="form-label">Email address</label>
 				<input type="email" name="email" class="form-control" placeholder="jane@doe.com">
 
+				<?php
+					if(isset($errorMsg[4])) {
+						foreach($errorMsg[4] as $emailErrors) {
+							echo "<p class='small text-danger'>".$emailErrors."</p>";
+						}
+					}
+				?>
+
 			</div>
 			<div class="mb-3">
 				<label for="password" class="form-label">Password</label>
 				<input type="password" name="password" class="form-control" placeholder="">
 				
+				<?php
+					if(isset($errorMsg[5])) {
+						foreach($errorMsg[5] as $passwordErrors) {
+							echo "<p class='small text-danger'>".$passwordErrors."</p>";
+						}
+					}
+				?>
+
 			</div>
 			<button type="submit" name="register_btn" class="btn btn-primary">Register Account</button>
 		</form>
@@ -89,4 +171,4 @@ if(isset($_REQUEST['register_btn'])){
 </html>
 
 
-<!--27:37-->
+<!--37:45-->
